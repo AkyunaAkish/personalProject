050145f94b366d5f6e0f1a6f3b75056db8ed2bc5
AkyunaAkish <akyunaakish@gmail.com>
2015-07-23 10:21:16 -0600

    changed favicon stuff

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieSession({
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
